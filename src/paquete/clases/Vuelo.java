3205b0e6be80ac7d40a890e8353a4474e8851adb
Leonardo <Leonardo@PcDeLeo>
2018-09-16 14:39:50 -0500

    Agregado clase Vuelo con atributos, constructor, seters y geters

---
+++ src/paquete/clases/Vuelo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete.clases;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Vuelo {
+    private String nomVuelo;
+    private int numAsientos;
+    private ListaEnlPasajero listaPasajeros;
+
+    public Vuelo(String nomVuelo, int numAsientos) {
+        this.nomVuelo = nomVuelo;
+        this.numAsientos = numAsientos;
+    }
+
+    public Vuelo(String nomVuelo, int numAsientos, ListaEnlPasajero listaPasajeros) {
+        this.nomVuelo = nomVuelo;
+        this.numAsientos = numAsientos;
+        this.listaPasajeros = listaPasajeros;
+    }
+
+    public String getNomVuelo() {
+        return nomVuelo;
+    }
+
+    public void setNomVuelo(String nomVuelo) {
+        this.nomVuelo = nomVuelo;
+    }
+
+    public int getNumAsientos() {
+        return numAsientos;
+    }
+
+    public void setNumAsientos(int numAsientos) {
+        this.numAsientos = numAsientos;
+    }
+
+    public ListaEnlPasajero getListaPasajeros() {
+        return listaPasajeros;
+    }
+
+    public void setListaPasajeros(ListaEnlPasajero listaPasajeros) {
+        this.listaPasajeros = listaPasajeros;
+    }
+    
+    
+    
+}
